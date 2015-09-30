51787726511527c7f4ffa49d5be2592cd58ff76c
Naviaux <bmw@bell.net>
2015-09-29 23:45:59 -0400

    Initial Commit

---
+++ get.php
@@ -0,0 +1,3 @@
+<?php
+	echo file_get_contents("http://api.steampowered.com/ISteamUserStats/GetGlobalStatsForGame/v0001/?appid=218620&count=3&name%5B0%5D=crimefest_challenge_chains_1&name%5B1%5D=crimefest_challenge_dallas_1&name%5B2%5D=crimefest_challenge_clover_1");
+?>
