6f23ec93cee30d2677511cb664af442d9c32dcec
Naviaux <bmw@bell.net>
2015-10-01 11:46:23 -0400

    RIP TWITTER

--- get.php
+++ get.php
@@ -1,3 +1,3 @@
 <?php
-	echo file_get_contents("http://api.steampowered.com/ISteamUserStats/GetGlobalStatsForGame/v0001/?appid=218620&count=5&name[0]=crimefest_challenge_chains_1&name[1]=crimefest_challenge_dallas_1&name[2]=crimefest_challenge_clover_1&name[3]=crimefest_challenge_clover_2&name[4]=crimefest_challenge_houston_2");
+	echo file_get_contents("http://api.steampowered.com/ISteamUserStats/GetGlobalStatsForGame/v0001/?appid=218620&count=5&name[0]=crimefest_challenge_chains_2&name[1]=crimefest_challenge_clover_1&name[2]=crimefest_challenge_dallas_1&name[3]=crimefest_challenge_dallas_2&name[4]=crimefest_challenge_houston_1");
 ?>
