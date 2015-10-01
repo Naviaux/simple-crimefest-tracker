03ff32aa3f39bddc195ac7275fe8e52d8f5cc8f6
Naviaux <bmw@bell.net>
2015-10-01 10:17:13 -0400

    cleaned up some more code

--- get.php
+++ get.php
@@ -1,6 +1,3 @@
 <?php
-	if ($_GET['site'] == "steam")
-		echo file_get_contents("http://api.steampowered.com/ISteamUserStats/GetGlobalStatsForGame/v0001/?appid=218620&count=5&name[0]=crimefest_challenge_chains_1&name[1]=crimefest_challenge_dallas_1&name[2]=crimefest_challenge_clover_1&name[3]=crimefest_challenge_clover_2&name[4]=crimefest_challenge_houston_2");
-	elseif ($_GET['site'] == "twitter")
-		echo file_get_contents("http://trehe.com/followers/petegold_.json");
+	echo file_get_contents("http://api.steampowered.com/ISteamUserStats/GetGlobalStatsForGame/v0001/?appid=218620&count=5&name[0]=crimefest_challenge_chains_1&name[1]=crimefest_challenge_dallas_1&name[2]=crimefest_challenge_clover_1&name[3]=crimefest_challenge_clover_2&name[4]=crimefest_challenge_houston_2");
 ?>
